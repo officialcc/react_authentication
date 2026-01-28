e84606a6a77ab340b88ad1fd3537b4f82e01bbc7
colin <163393924+officialcc@users.noreply.github.com>
2026-01-28 18:19:13 +0800

    Lesson 391 incomplete

--- frontend/src/pages/Authentication.js
+++ frontend/src/pages/Authentication.js
@@ -5,3 +5,16 @@ function AuthenticationPage() {
 }
 
 export default AuthenticationPage;
+
+export async function action({request}) {
+  const searchParams = new URL(request.url).searchParams;
+  const mode = searchParams.get('mode') || 'login';
+
+  const data = await request.formData();
+  const authData = {
+    email: data.get('email'),
+    password: data.get('password'),
+  }
+
+  fetch('http://localhost:8080/')
+}
